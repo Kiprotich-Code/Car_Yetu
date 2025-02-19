4d8da6f6c36692466f353a7397201def5a133a36
Kiprotich-Code <fleeklawi@gmail.com>
2025-02-19 17:19:57 +0300

    build files update

--- dashboard/build_files.sh
+++ dashboard/build_files.sh
@@ -1,4 +1,6 @@
 # build_files.sh
 
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate
 python3.9 manage.py collectstatic --noinput
