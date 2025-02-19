2a84a56cbe37d702deb6a3f3e8d60e15fdc131f7
Kiprotich-Code <fleeklawi@gmail.com>
2025-02-19 17:26:14 +0300

    build files update

--- dashboard/build_files.sh
+++ dashboard/build_files.sh
@@ -1,6 +1,10 @@
 # build_files.sh
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
 
-python3 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+# activate the virtual environment
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
